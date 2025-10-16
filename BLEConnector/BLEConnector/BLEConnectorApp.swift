3d4976717e986103306347224a0be7aeaa2a6743
Oksana Dionisieva <dionisieva@sandiplus.com>
2025-10-16 11:47:39 +0300

    [TASK-1] - Added project folder

---
+++ BLEConnector/BLEConnector/BLEConnectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEConnectorApp.swift
+//  BLEConnector
+//
+//  Created by Oksana Dionisieva on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BLEConnectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
